be24d1a3b6533619707d51b8b4ce7fa63ec22e87
sumruti <46131919+sumruti@users.noreply.github.com>
2018-12-27 16:42:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ app.post('/PdfDetails', function(req, res) {
 		    // The directory the file gets written into if not using .toFile(filename, callback). default: '/tmp'
 		 
 		  // Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html
-		  height: "300mm",        // allowed units: mm, cm, in, px
+		  height: "270mm",        // allowed units: mm, cm, in, px
 		  width: "212mm",
 		  base: path2
 		}
