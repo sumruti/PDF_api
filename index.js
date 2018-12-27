f48c80b17ba429dab61238bb7bf9b24f9c62245a
sumruti <46131919+sumruti@users.noreply.github.com>
2018-12-27 16:24:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.post('/PdfDetails', function(req, res) {
 		 
 		  // Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html
 		  height: "212mm",        // allowed units: mm, cm, in, px
-		  width: "195mm",
+		  width: "230mm",
 		  base: path2
 		}
 
